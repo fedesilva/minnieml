86b8e740a5708996e07ee201e4366aaba3bfb902
federico silva <fede.silva@gmail.com>
2021-07-30 20:33:23 -0700

    change print format

--- modules/mmlc-lib/src/main/scala/mml/mmlclib/util/yolo/ParseTreeInspector.scala
+++ modules/mmlc-lib/src/main/scala/mml/mmlclib/util/yolo/ParseTreeInspector.scala
@@ -2,16 +2,17 @@ package mml.mmlclib.util.yolo
 
 import mml.mmlclib.api.ParseContext
 import org.antlr.v4.runtime.ParserRuleContext
-import org.antlr.v4.runtime.tree.{ParseTree, TerminalNode}
+import org.antlr.v4.runtime.tree.{ParseTree, TerminalNode, ErrorNode}
 
 import scala.annotation.tailrec
+import mml.mmlclib.antlr.parser.MinnieMLParser.{IdContext, TpIdContext}
 
 object ParseTreeInspector {
 
   case class NodeInfo(
     depth: Int,
-    cls: String,
-    tree: ParseTree
+    cls:   String,
+    tree:  ParseTree
   )
 
   type Nodes = Seq[NodeInfo]
@@ -26,26 +27,26 @@ object ParseTreeInspector {
       trees match {
 
         case (t: ParserRuleContext) :: tail if t.getChildCount == 0 =>
-          val cls = t.getClass.getName
+          val cls = t.getClass.getSimpleName()
           val depth = t.getRuleContext().depth
 
           loop(tail, nodes :+ NodeInfo(depth, cls, t))
 
         case (t: ParserRuleContext) :: tail if t.getChildCount > 0 =>
-          val cls = t.getClass.getName
+          val cls = t.getClass.getSimpleName()
           val depth = t.getRuleContext().depth
           val children = t.children.asScala.toList
 
           loop(children ++ tail, nodes :+ NodeInfo(depth, cls, t))
 
         case (t: TerminalNode) :: tail =>
-          val cls = t.getClass.getName + " " + t.getSymbol
+          val cls = t.getClass.getSimpleName() + " " + t.getSymbol
           val depth = parentDepth(t) + 1
 
           loop(tail, nodes :+ NodeInfo(depth, cls, t))
 
         case t :: tail =>
-          val cls = t.getClass.getName
+          val cls = t.getClass.getSimpleName()
           val depth = parentDepth(t) + 1
 
           loop(tail, nodes :+ NodeInfo(depth, cls, t))
@@ -58,34 +59,51 @@ object ParseTreeInspector {
     def parentDepth(current: ParseTree): Int = {
       Option(current.getParent) match {
         case Some(p: ParserRuleContext) => p.getRuleContext().depth()
-        case Some(pt: ParseTree)        => parentDepth(pt)
-        case None                       => 0
+        case Some(pt: ParseTree) => parentDepth(pt)
+        case None => 0
       }
     }
 
     loop(List(tree), Vector())
-
+ 
   }
 
   def print(nodes: Nodes): Unit = nodes foreach {
 
+    case NodeInfo(depth, cls, t: ErrorNode) =>
+      println(s" ${" " * depth} $cls : ${t.getText()}")
+
     case NodeInfo(depth, cls, t: TerminalNode) =>
-      println(s" ${" " * depth} |-- $cls, ${t.getSymbol}")
+      println(s" ${" " * depth} $cls, ${t.getSymbol.getText()}")
+
+    case NodeInfo(depth, cls, t: IdContext) =>
+      println(s" ${" " * depth} $cls : ${t.getRuleContext().getText()}")
+
+    case NodeInfo(depth, cls, t: TpIdContext) =>
+      println(s" ${" " * depth} $cls : ${t.getRuleContext().getText()}")
 
     case NodeInfo(depth, cls, _) =>
-      println(s" ${" " * depth} |-- $cls")
+      println(s" ${" " * depth} $cls")
 
   }
 
   def flattenAndPrint[T <: ParserRuleContext](ctx: ParseContext[T]): Unit =
     flatten(ctx.tree) |> print
 
+  /** Apply an effectful partial function to the list of nodes `orElse` use a default
+    *
+    * @param nodes
+    * @param pf
+    */
   def inspectCustom(nodes: Nodes)(pf: PartialFunction[NodeInfo, Unit]): Unit = {
 
     val default: PartialFunction[NodeInfo, Unit] = {
 
       case NodeInfo(depth, cls, t: TerminalNode) =>
-        println(s" ${" " * depth}  $depth -> $cls : ${t.getSymbol}")
+        println(s" ${" " * depth}  $depth -> $cls : ${t.getSymbol.getText()}")
+
+      case NodeInfo(depth, cls, t: ParserRuleContext) =>
+        println(s" ${" " * depth}  $depth -> $cls : ${t.toStringTree()}")
 
       case NodeInfo(depth, cls, _) =>
         println(s" ${" " * depth}  $depth -> $cls")
