6b4ac1d7904185e2199e910baf626bbde321ca0a
fede silva <fede.silva@gmail.com>
2025-02-16 16:37:25 -0800

    wip, broken

--- modules/mmlc-lib/src/main/scala/mml/mmlclib/semantic/ExpressionBloomer.scala
+++ modules/mmlc-lib/src/main/scala/mml/mmlclib/semantic/ExpressionBloomer.scala
@@ -1,9 +1,97 @@
 package mml.mmlclib.semantic
 
 import mml.mmlclib.ast.*
+import cats.syntax.all.*
 
 object ExpressionBloomer:
-  private def injectStandardOperators(module: Module): Module =
+
+  enum RewriteError:
+    case UndefinedRef(ref: Ref, member: Member)
+    case UndefinedOperator(ref: Ref)
+
+  // Top-level: rewrite members (Bnd, FnDef and OpDef) in a module.
+  // We will start with simple operations on expressions.
+  // TODO
+  //  - Implement rewriteBnd
+  //  - Implement binary operators
+  //  - Implement prefix operators
+  //  - Implement postfix operators
+  //  - Implement function calls (same as prefix operators)
+  //  - Implement rewriteFnDef
+  //  - Implement rewriteOpDef
+  def rewriteModule(module: Module): Either[List[RewriteError], Module] =
+
+    val newModule = injectStandardOperators(module)
+
+    // Validate all references in the module.
+    val badRefs = validateAllRefs(newModule)
+    if badRefs.nonEmpty then badRefs.asLeft
+    else
+      val newMembers = newModule.members.map {
+        case bnd: Bnd => rewriteBnd(bnd)
+        // case fnDef: FnDef => rewriteFnDef(fnDef)
+        // case opDef: OpDef => rewriteOpDef(opDef)
+        case other => other
+      }
+      ???
+
+  // Rewrite a binding.
+  def rewriteBnd(bnd: Bnd): Bnd =
+    // A binding is a simple expression, so we can rewrite it directly.
+    // We will start by rewriting the expression.
+    ???
+
+  // Rewrite an expression.
+  def rewriteExpr(expr: Expr): Expr =
+    // An expression is a list of terms, so we can rewrite each term.
+    // We will start by rewriting the terms.
+    ???
+
+  def validateAllRefs(module: Module): List[RewriteError] =
+    module.members.flatMap {
+      case bnd: Bnd =>
+        collectBadRefs(bnd.value, module).map(RewriteError.UndefinedRef(_, bnd))
+      case fnDef: FnDef =>
+        collectBadRefs(fnDef.body, module).map(RewriteError.UndefinedRef(_, fnDef))
+      case opDef: OpDef =>
+        collectBadRefs(opDef.body, module).map(RewriteError.UndefinedRef(_, opDef))
+    }
+
+  def collectBadRefs(expr: Expr, module: Module): List[Ref] =
+    expr.terms.foldLeft(List.empty[Ref]) {
+      case (acc, ref: Ref) =>
+        if lookupRef(ref, module).isDefined then ref :: acc
+        else acc
+      case (acc, group: GroupTerm) =>
+        acc ++ collectBadRefs(group.inner, module)
+      case (acc, expr: Expr) =>
+        acc ++ collectBadRefs(expr, module)
+    }
+
+  def isRefOp(ref: Ref, module: Module): Option[OpDef] =
+    lookupRef(ref, module) match {
+      case o: OpDef => o.some
+      case _ => None
+    }
+
+  def isRefFn(ref: Ref, module: Module): Option[FnDef] =
+    lookupRef(ref, module) match {
+      case f: FnDef => f.some
+      case _ => None
+    }
+
+  // a ref is undefined if the name is not part of the modules members
+  // TODO when we have imports, look there, too.
+  def lookupRef(term: Ref, module: Module): Option[Member] =
+    module.members.find {
+      case bnd:   Bnd => bnd.name == term.name
+      case fnDef: FnDef => fnDef.name == term.name
+      case opDef: OpDef => opDef.name == term.name
+      case _ => false
+    }
+
+  // Prepend standard operator definitions to the module.
+  def injectStandardOperators(module: Module): Module =
     val standardOps = List(
       ("+", 3),
       ("-", 3),
@@ -17,6 +105,7 @@ object ExpressionBloomer:
         param1     = FnParam(dummySpan, "a"),
         param2     = FnParam(dummySpan, "b"),
         precedence = prec,
+        assoc      = Associativity.Left,
         body       = Expr(dummySpan, List(Hole(dummySpan))),
         typeSpec   = None,
         typeAsc    = None,
@@ -24,43 +113,3 @@ object ExpressionBloomer:
       )
     }
     module.copy(members = standardOps ++ module.members)
-
-  def rewriteModule(module: Module): Module =
-    val moduleWithOps = injectStandardOperators(module)
-    val operators = moduleWithOps.members.foldLeft(Map.empty[String, Int]) {
-      case (ops, BinOpDef(_, name, _, _, prec, _, _, _, _)) => ops + (name -> prec)
-      case (ops, UnaryOpDef(_, name, _, prec, _, _, _, _, _)) => ops + (name -> prec)
-      case (ops, _) => ops
-    }
-
-    def rewriteExpr(expr: Expr): Expr =
-      def precedence(term: Term): Int = term match
-        case Ref(_, name, _, _) => operators.getOrElse(name, 0)
-        case _ => 0
-
-      def groupAtPrecedence(terms: List[Term], targetPrec: Int): List[Term] =
-        terms match
-          case left :: op :: right :: rest if precedence(op) == targetPrec =>
-            val grouped = GroupTerm(
-              SourceSpan(left.span.start, right.span.end),
-              Expr(expr.span, List(left, op, right))
-            )
-            groupAtPrecedence(grouped :: rest, targetPrec)
-          case t :: rest => t :: groupAtPrecedence(rest, targetPrec)
-          case Nil => Nil
-
-      val allPrecedences = operators.values.toSet.toList.sorted
-      val rewrittenTerms = allPrecedences.foldLeft(expr.terms) { (terms, prec) =>
-        groupAtPrecedence(terms, prec)
-      }
-
-      Expr(expr.span, rewrittenTerms, expr.typeSpec)
-
-    def rewriteMember(member: Member): Member = member match
-      case bnd @ Bnd(_, _, expr, _, _, _) =>
-        bnd.copy(value = rewriteExpr(expr))
-      case fn @ FnDef(_, _, _, body, _, _, _) =>
-        fn.copy(body = rewriteExpr(body))
-      case other => other
-
-    module.copy(members = module.members.map(rewriteMember))
--- scripts/rewrite1.scala
+++ scripts/rewrite1.scala
@@ -13,8 +13,12 @@ def rewrite(src: String) =
   parseModule(src) match {
     case Some(module) =>
       println(s"Original module: \n${prettyPrintAst(module)} ")
-      val member          = module.members.head
-      val rewrittenModule = ExpressionBloomer.rewriteModule(module)
-      println(s"Rewritten module: \n${prettyPrintAst(rewrittenModule)} ")
+      val member = module.members.head
+      ExpressionBloomer.rewriteModule(module) match {
+        case Right(mod) =>
+          println(s"Rewritten module: \n${prettyPrintAst(mod)} ")
+        case Left(err) =>
+          println(s"Error: $err")
+      }
     case None => println("Failed to parse module")
   }
