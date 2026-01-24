f3e730cdcf7dde34a3d22b6cf091e00511a0ff14
federico silva <fedesilva@users.noreply.github.com>
2026-01-23 23:13:24 -0800

    put indexes in module.scla

--- modules/mmlc-lib/src/main/scala/mml/mmlclib/ast/module.scala
+++ modules/mmlc-lib/src/main/scala/mml/mmlclib/ast/module.scala
@@ -11,3 +11,26 @@ case class Module(
 ) extends AstNode,
       FromSource,
       Member
+
+
+/** Index for soft reference lookups. Stores resolvables and resolvable types by their stable IDs.
+	* Updated alongside AST rewrites to always point to the current version of each node.
+	*/
+case class ResolvablesIndex(
+	resolvables:     Map[String, Resolvable]     = Map.empty,
+	resolvableTypes: Map[String, ResolvableType] = Map.empty
+):
+	def lookup(id:     String): Option[Resolvable]     = resolvables.get(id)
+	def lookupType(id: String): Option[ResolvableType] = resolvableTypes.get(id)
+
+	def updated(r: Resolvable): ResolvablesIndex =
+		r.id.fold(this)(id => copy(resolvables = resolvables.updated(id, r)))
+
+	def updatedType(t: ResolvableType): ResolvablesIndex =
+		t.id.fold(this)(id => copy(resolvableTypes = resolvableTypes.updated(id, t)))
+
+	def updatedAll(rs: Iterable[Resolvable]): ResolvablesIndex =
+		rs.foldLeft(this)((idx, r) => idx.updated(r))
+
+	def updatedAllTypes(ts: Iterable[ResolvableType]): ResolvablesIndex =
+		ts.foldLeft(this)((idx, t) => idx.updatedType(t))
